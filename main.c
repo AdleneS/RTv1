cfdb0739f8bb8ea0c19c886079ee225959af7aa6
Adlene Saba <ad25400@hotmail.fr>
2020-01-28 17:30:57 +0100

    Add Rot

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ void		addsphere(t_param *p)
 	t_sphere		sp1;
 	t_sphere		sp2;
 	t_sphere		sp3;
+	t_sphere		sp4;
 
 	sp1.pos = (t_vec3df){0., 0., -50};
 	sp1.radius = 3;
@@ -22,24 +23,29 @@ void		addsphere(t_param *p)
 	sp2.radius = 3;
 	sp2.color = 0x00FF00;
 	sp3.pos = (t_vec3df){1.5, 0.8, 0};
-	sp3.radius = 1.5;
+	sp3.radius = 5;
 	sp3.color = 0x0000FF;
+	sp4.pos = (t_vec3df){0.0, 0.0, 0.0};
+	sp4.radius = 100;
+	sp4.color = 0xFFFFFF;
 	sphere_pushback(&p->obj.sp, sp1, p);
 	sphere_pushback(&p->obj.sp, sp2, p);
-	// sphere_pushback(&p->obj.sp, sp3, p);
+	sphere_pushback(&p->obj.sp, sp3, p);
+	sphere_pushback(&p->obj.sp, sp4, p);
 
 }
 
 int main (int argc, char *argv[])
 {
 	t_param			*p;
-
+	t_vec3df		move;
 	p = (t_param *)malloc(sizeof(t_param));
 
 	SDL_Init(SDL_INIT_VIDEO);
 
 	p->cam.pos = (t_vec3df){0.0, 0.0, -100.0};
 	p->cam.rot = (t_vec3df){0.0, 0.0, 0.0};
+	
 	p->sdl.win = SDL_CreateWindow("RTV1starfoulilaliloulala", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 	p->sdl.ren = SDL_CreateRenderer(p->sdl.win, -1, 0);
 	p->sdl.tex = SDL_CreateTexture(p->sdl.ren, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
@@ -54,31 +60,36 @@ int main (int argc, char *argv[])
 		SDL_Event		event;
 		if (SDL_PollEvent(&event))
 		{
+			move = (t_vec3df){0.0, 0.0, 0.0};
 			if (event.type == SDL_QUIT)
 				break;
 			if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
 			{
 				if (state[SDL_SCANCODE_D])
-					p->cam.pos = v_add(p->cam.pos, (t_vec3df){1, 0, 0});
-				if (state[SDL_SCANCODE_Q])
-					p->cam.pos = v_add(p->cam.pos, (t_vec3df){-1, 0, 0});
+					move = v_add(move, (t_vec3df){1, 0, 0});
+				if (state[SDL_SCANCODE_A])
+					move = v_add(move, (t_vec3df){-1, 0, 0});
 				if (state[SDL_SCANCODE_S])
-					p->cam.pos = v_add(p->cam.pos, (t_vec3df){0, 0, -1});
-				if (state[SDL_SCANCODE_Z])
-					p->cam.pos = v_add(p->cam.pos, (t_vec3df){0, 0, 1});
+					move = v_add(move, (t_vec3df){0, 0, -1});
+				if (state[SDL_SCANCODE_W])
+					move = v_add(move, (t_vec3df){0, 0, 1});
 				if (state[SDL_SCANCODE_LSHIFT])
-					p->cam.pos = v_add(p->cam.pos, (t_vec3df){0, 1, 0});
+					move = v_add(move, (t_vec3df){0, 1, 0});
 				if (state[SDL_SCANCODE_SPACE])
-					p->cam.pos = v_add(p->cam.pos, (t_vec3df){0, -1, 0});
+					move = v_add(move, (t_vec3df){0, -1, 0});
 				if (state[SDL_SCANCODE_R])
 					p->cam.rot = v_add(p->cam.rot, (t_vec3df){0.05,0, 0});
 				if (state[SDL_SCANCODE_F])
 					p->cam.rot = v_add(p->cam.rot, (t_vec3df){-0.05, 0, 0});
-				if (state[SDL_SCANCODE_A])
-					p->cam.rot = v_add(p->cam.rot, (t_vec3df){0, 0.05, 0});
 				if (state[SDL_SCANCODE_E])
+					p->cam.rot = v_add(p->cam.rot, (t_vec3df){0, 0.05, 0});
+				if (state[SDL_SCANCODE_Q])
 					p->cam.rot = v_add(p->cam.rot, (t_vec3df){0, -0.05, 0});
+				
 			}
+			rot_x(p, &move);
+			rot_y(p, &move);
+			p->cam.pos = v_add(p->cam.pos, move);
 		}
 		ray_tracing(p);
 		SDL_UpdateTexture(p->sdl.tex, NULL, p->sdl.pixels, WIDTH * 4);
